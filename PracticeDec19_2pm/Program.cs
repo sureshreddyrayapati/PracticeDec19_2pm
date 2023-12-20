e340beffb73f2831783b2a5f37ac0bdb7accab7d
sureshreddyrayapati <resuresh0@gmail.com>
2023-12-20 20:14:20 +0530

    Add project files.

---
+++ PracticeDec19_2pm/Program.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Services;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeDec19_2pm
+{
+    class MyError:ApplicationException
+    {
+        public void WaterOverflowException()
+        {
+            Console.WriteLine("Exception Happend Tank is full");
+        }
+    }
+    
+    internal class Program
+    {
+        public void water()
+        {
+            try
+            {
+                Console.WriteLine("How meny liters you want to fill your tank");
+                int w = Convert.ToInt32(Console.ReadLine());
+                if (w >= 100)
+                {
+                    throw new MyError();
+                }
+            }
+            catch (MyError e)
+            {
+                    e.WaterOverflowException();
+            }
+        }
+        private string pass="Suresh@123";
+        private string email = "resuresh0@gmail.com";
+        private long ph = 6302539493;
+        enum Seson { summer=1, rainy,winter }
+        enum Months { jan,feb,march}
+        void set1(string p,string a,long ps)
+        {
+            this.ph = ps;
+            this.email = a;
+            this.pass = p; 
+        }
+        string getpass() { return pass; }
+        string getemail() { return email; }
+        long getph() { return ph; }
+        string M1(int a)
+        {
+            Console.WriteLine("Method M1");
+            Console.WriteLine(a);
+            return "Method 1";
+        }
+        String M1(string a)
+        {
+            Console.WriteLine("Method M2");
+            Console.WriteLine(a);
+            return "Method 2";
+        }
+        int M1()
+        {
+            Console.WriteLine("Method M3");
+            return 3;
+        }
+        static void Main(string[] args)
+        {
+            Program x= new Program();
+             Console.WriteLine( x.M1(x.M1(x.M1())));
+            Console.WriteLine(x.getemail() );
+             Console.WriteLine(x.getpass());
+             Console.WriteLine(x.getph());
+             x.set1("Rayapati@123", "rayapati.reddy@mphasis.com", 9502208854);
+             Console.WriteLine(x.getemail());
+             Console.WriteLine(x.getpass());
+             Console.WriteLine(x.getph());
+              exceptionExample();
+             Seson s1 = Seson.summer;
+             Console.WriteLine(s1);
+             int n1=(int)Seson.winter;
+             Console.WriteLine(n1);
+             Student st = new Student();
+             st.display();
+            x.water();
+        }
+        struct Student
+        {
+            public int id;
+            public string name;
+            public int age;
+            public void display()
+            {
+                id = 100;
+                name = "Naga Suresh Reddy";
+                age = 22;
+                Console.WriteLine($"Student Id={id}\nStudent Name{name}\nStudent age={age}");
+               
+            }
+        }
+       
+        static void exceptionExample()
+        {
+            try
+            {
+                Console.WriteLine("Enter a abd b values");
+                int a, b;
+                a=Convert.ToInt32(Console.ReadLine());
+                b=Convert.ToInt32(Console.ReadLine());
+                int c = a / b;
+                Console.WriteLine(c);
+                try
+                {
+                    int a1=Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine(a1);
+                }
+                catch (Exception x)
+                {
+                    Console.WriteLine(x);
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e);
+                try
+                {
+                    int n = Convert.ToInt32(Console.ReadLine());
+                    int[] arr = new int[n];
+                    for (int i = 0; i < arr.Length; i++)
+                    {
+                        arr[i] = Convert.ToInt32(Console.ReadLine());
+                    }
+                    for (int i = 0; i <= i; i++)
+                    {
+                        Console.WriteLine(arr[i]);
+                    }
+                }
+                catch (Exception y)
+                {
+                    Console.WriteLine(y);
+                }
+            }
+        }
+
+    }
+}
---
+++ PracticeDec19_2pm/multiexception.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeDec19_2pm
+{
+    internal class multiexception
+    {
+        public static double CalCulations(double v1, double v2, char ch)
+        {
+            double result = 0.0;
+            switch (ch)
+            {
+                case '+': { result = v1 + v2; break; }
+                case '-':
+                    { result = v1 - v2; break; }
+                case '*':
+                    {
+                        result = v1 * v2; break;
+                    }
+                case '/':
+                    {
+                        result = v1 / v2; break; }
+
+            }
+            return result;//local 
+        }
+        static void Main(string[] args)
+        {
+            double num1, num2;
+            double Result = 0;
+            char choice;
+            try
+            {
+                Console.WriteLine("enter first number");
+
+                num1 = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("please eneter choice +  or - or * or /");
+
+                choice = char.Parse(Console.ReadLine());
+
+                if (choice != '+' && choice != '-' && choice != '*' && choice != '/')
+                {
+                    throw new Exception(choice.ToString());
+                }
+                Console.WriteLine("enter 2 number");
+                num2 = double.Parse(Console.ReadLine());
+                if (choice == '/') if (num2 == 0)
+                    {
+                        throw new DivideByZeroException(" divisin by zero is nor allowed");
+                    }
+                Result = CalCulations(num1, num2, choice);
+
+                Console.WriteLine("\n {0} \t {1} \t {2} = {3} ", num1, choice, num2, Result);
+            }
+            catch (FormatException fe)
+            {
+                Console.WriteLine("the numbers we typed is not valid"); Console.WriteLine(fe.Message);
+            }
+
+            catch (DivideByZeroException de)
+            {
+                Console.WriteLine(de.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(" choice error {0} not a valid choice ", ex.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
